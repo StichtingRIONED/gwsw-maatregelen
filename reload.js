28fdd9c54801c780a1dc1f9c917ebaa6c9e5b261
Wouter van Riel <wouter.van.riel@infralytics.org>
2024-02-23 11:55:01 +0100

    pagina ververs

---
+++ reload.js
@@ -0,0 +1,30 @@
+const itv = 2 * 1000; // Frequentie check op aanpassingen
+const port = 3010; // Sync met server
+
+let body = document.getElementById("body");
+let timerId = setInterval(chkUpdate, itv);
+
+if (localStorage.getItem('scrollpos')) {
+  body.hidden = true; // Voorkom onrustige opbouw
+  setTimeout(scrollIt, 500); // Vertraag: laat document volledig laden
+}
+function chkUpdate() {
+  fetch(`http://localhost:${port}/reload`).then((res) => {
+
+    if (res.status == 200) {
+      localStorage.setItem('scrollpos', window.scrollY);
+      document.location.hash = ""; // Verwijder hst-link
+      document.location.reload();
+    }
+
+  }).catch((err) => { // Geen localhost, publicatie-fase? Stop update-check
+    clearTimeout(timerId);
+  });
+}
+function scrollIt() {
+  const y = 0;  //350; // offset bij scrolling (bij hidden-body offset niet meer nodig)
+  var scrollpos = localStorage.getItem('scrollpos');
+  body.hidden = false; // Nu zichtbaar maken, anders werkt scroll niet
+  window.scrollTo({ top: scrollpos - y, left: 0, behavior: "instant" });
+  localStorage.removeItem('scrollpos');
+}
